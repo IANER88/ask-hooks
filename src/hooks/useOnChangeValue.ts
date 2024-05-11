374dd709ac7ee41edde8ded4d6900b7516875645
L.Jay <bozhancom@163.com>
2024-05-11 21:16:16 +0800

    fix: update onChangeValue

--- src/hooks/useOnChangeValue.ts
+++ src/hooks/useOnChangeValue.ts
@@ -9,23 +9,23 @@ type SetStateAction<S> = (value: S) => void;
 
 type UseOnChangeValue<S> = [Method<S>, Dispatch<SetStateAction<S>>];
 
-type onChangeType<S> = ((value: S) => S) | ((value: S) => Promise<S>)
+type transformType<S> = ((value: S) => S) | ((value: S) => Promise<S>)
 /**
  * 简化 onChange -> value
  * @param initialState 初始值
- * @param onChange 转换值的方法（可以在这做一些转换操作）
+ * @param transform 转换值的方法（可以在这做一些转换操作）
  * @returns []
  */
 export default function useOnChangeValue<S>(
   initialState?: S,
-  onChange?: onChangeType<S>,
+  transform?: transformType<S>,
 ): UseOnChangeValue<S> {
   const [value, setValue] = useState(initialState);
   const method = {
     value,
     onChange: async (value: S) => {
-      if (typeof onChange === 'function') {
-        setValue(await onChange(value));
+      if (typeof transform === 'function') {
+        setValue(await transform(value));
       } else {
         setValue(value);
       }
