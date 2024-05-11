045675fcffb55c07b75ff1b2128f74b54035ad64
luminjie <luminjie@byteawake.com>
2024-05-11 17:31:24 +0800

    fix: update useOnChangeValue type

--- src/hooks/useOnChangeValue.ts
+++ src/hooks/useOnChangeValue.ts
@@ -8,14 +8,17 @@ type Method<S> = {
 type SetStateAction<S> = (value: S) => void;
 
 type UseOnChangeValue<S> = [Method<S>, Dispatch<SetStateAction<S>>];
+
+type onChangeType<S> = ((value: S) => S) | ((value: S) => Promise<S>)
 /**
  * 简化 onChange -> value
  * @param initialState 初始值
+ * @param onChange 转换值的方法（可以在这做一些转换操作）
  * @returns []
  */
 export default function useOnChangeValue<S>(
   initialState?: S,
-  onChange?: (value: S) => S,
+  onChange?: onChangeType<S>,
 ): UseOnChangeValue<S> {
   const [value, setValue] = useState(initialState);
   const method = {
