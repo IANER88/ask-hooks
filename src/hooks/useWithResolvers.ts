1092a601d177259a118743a275d82fce7c07880f
luminjie <luminjie@byteawake.com>
2024-05-13 11:29:38 +0800

    fix: add useWithresolvers

---
+++ src/hooks/useWithResolvers.ts
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+
+type Resolve<S> = ((value?: S) => void) | null;
+
+const withResolvers = <S>() => {
+  let resolve: Resolve<S> = () => void 0;
+  let reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return {
+    promise,
+    resolve,
+    reject,
+  };
+};
+
+/**
+ * 等待成功的异步
+ * useWithResolvers
+ * @returns 
+ */
+
+export default function useWithResolvers<S>() {
+  const promise = useRef(withResolvers<S>());
+  return {
+    promise: promise.current.promise,
+    resolve: (value: S) => {
+      promise.current?.resolve?.(value);
+      promise.current = withResolvers();
+    },
+    reject: promise?.current.reject,
+  };
+}
