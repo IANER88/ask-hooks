045f8df055ff2bfa141fcf36085272c8b931d9d3
L.Jay <bozhancom@163.com>
2024-08-18 17:16:04 +0800

    fix: add useGetResetInitialSetState

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import {
   useWithResolvers,
   useOnChangeValue,
   useFrequencyEffect,
+  useGetResetInitialSetState,
 } from '../src/hooks';
 
 export {
   useWithResolvers,
   useOnChangeValue,
   useFrequencyEffect,
+  useGetResetInitialSetState
 }
